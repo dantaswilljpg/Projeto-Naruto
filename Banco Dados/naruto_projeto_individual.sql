50c5e7c733df42d00ad8a1a187802fdee2c0116c
dantaswill748@gmail.com <dantaswill748@gmail.com>
2023-05-20 22:09:24 -0300

    Implementando nova tela pós login, para opção de escolha para realizar quiz ou dashboard, arrumando detalhes do front das telas

--- Banco Dados/naruto_projeto_individual.sql
+++ Banco Dados/naruto_projeto_individual.sql
@@ -1,4 +1,5 @@
-DROP DATABASE naruto_projeto_individual;
+-- DROP DATABASE naruto_projeto_individual;
+
 
 CREATE DATABASE naruto_projeto_individual;
 
@@ -11,22 +12,19 @@ CREATE TABLE Usuario (
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(100) NOT NULL,
    fkpersonagem INT ,
-   FOREIGN KEY (fkpersonagem) REFERENCES personagem(idPersonagem)
+   FOREIGN KEY (fkpersonagem) REFERENCES Personagem (idPersonagem)
 );
-INSERT INTO Usuario VALUES
-(NULL , 'Sophia Amaral','SophiaAmaral@gmail.com','sophia123',6);
 
 
 INSERT INTO Usuario VALUES
-(NULL , 'Luigi Tadeu','Luigi@gmail.com','Linguincina',6),
-(NULL,'Will Dantas','Dantas@gmail.com','UchihaMadara12345',1),
-(NULL,'Eduardo Verri','Eduardo@gmail.com','Eduardo123',2),
-(NULL,'Vivian Silva','Vivian@gmail.com','Vivian123',3),
-(NULL,'Fernanda Caramico','Fernanda@gmail.com','Fernanda123',4),
-(NULL,'Monica Herrera','Monica@gmail.com','Monica123',5),
-(NULL,'Caio Algorotimo','Caio@gmail.com','Caio123',1);
-
-
+(NULL,' Fernanda Caramico ','Caramico@gmail.com.br','12345678910',3),
+(NULL,' Eduardo Veri ','Edu@gmail.com.br','12345678910',4),
+(NULL,' Monica Herrero','Monica@gmail.com.br','12345678910',5),
+(NULL,' Caio Lima ','Caiogmail.com.br','12345678910',6),
+(NULL,' João Paula ','JP@gmail.com.br','12345678910',7),
+(NULL,'Erik Roberto','Vivians@gmail.com.br','12345678910',8),
+(NULL,' Vivian Silva ','Vivians@gmail.com.br','12345678910',9),
+(NULL,'teste123','teste@gmail.com.br','12345678',10);
 
 
 
@@ -53,30 +51,46 @@ INSERT INTO Personagem VALUES
 (NULL,'Hatake Kakashi'),
 (NULL,'Gaara Deserto'),
 (NULL,'Neji Hyugaa'),
-(NULL,'Jiraya Sama');
-   
+(NULL,'Rock Lee'),
+(NULL,'Gaara Deserto'),
+(NULL,'Sakura'),
+(NULL,'Hinata Hyugaa');   
 
 SELECT * FROM Usuario;
 SELECT * FROM  Pontuacao;
-SELECT * FROM Personagem;
+SELECT *FROM Personagem;
+
+
+
+-- SELECT DE MÉDIA DO QUIZ E SOMA
+SELECT fkUsuario, ROUND(AVG(pontuacao), 2) AS Média, ROUND(SUM(pontuacao), 2) AS Soma FROM Pontuacao GROUP BY fkUsuario;
+
+
+
+
+-- SELECT DE MENOR PERCENTUAL DO QUIZ E MAIOR PERCENTUAL
+SELECT fkUsuario, MIN(percentualPontuacao) AS MenorPercentual, MAX(percentualPontuacao) AS MaiorPercentual FROM Pontuacao GROUP BY fkUsuario;
 
 
 
 
 
-SELECT nome, pontuacao,  percentualPontuacao FROM Pontuacao JOIN usuario ON fkUsuario = idUsuario ORDER BY  percentualPontuacao  DESC;
+-- SELECT DE NOME, PONTUACAO E PERCENTUAL DO QUIZ--
+SELECT nome, pontuacao,  percentualPontuacao FROM Pontuacao JOIN usuario ON fkUsuario = idUsuario ORDER BY  pontuacao  DESC;
 
 
+
+
+
+-- SELECT (DASHBOARD) PEGAR PERSONAGEM FAVORITOS DE CADA USUARIO
 SELECT COUNT(Usuario.fkPersonagem) as voto, Personagem.nomePersonagem AS personagem 
 FROM Usuario JOIN Personagem ON  personagem.idPersonagem = usuario.fkPersonagem group by usuario.fkPersonagem;
 
 
-SELECT COUNT(Pontuacao.fkUsuario) as Nome, Pontuacao.percentualPontuacao AS Média
-FROM Pontuacao JOIN Usuario ON   Pontuacao.idPontuacao = Pontuacao.fkUsuario group by Pontuacao.fkUsuario;
 
 
 
-select  round(avg(pontuacao.percentualPontuacao) , 2) as 'Média do quiz' from Pontuacao;
+-- SELECT (DASHBOARD) PEGAR MÉDIA DO QUIZ
+SELECT round(AVG(pontuacao.percentualPontuacao) , 2) AS 'Média do quiz' FROM Pontuacao;
 
 
-select round(avg(pontuacao.percentualPontuacao)) from Pontuacao;
