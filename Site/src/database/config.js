1e7902c80e06be73114b2254f24489fa701fd0cd
dantaswilljpg <dantaswill748@gmail.com>
2023-06-03 16:56:34 -0300

    Alterando estilização da tela de cadastro

--- Site/src/database/config.js
+++ Site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "naruto_projeto_individual",
     user: "root",
-    password: "#Gf49773886824",
+    password: "",
 };
 
 function executar(instrucao) {
